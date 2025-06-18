4e107ae7d29eb703f4b0e73ad0b31b587084d6ad
Omar Bucio <omarbucio2020@gmail.com>
2025-06-18 18:00:57 -0500

    Set up db conn with dotenv and added .env to .gitignore

--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const mysql = require("mysql");
 const app = express();
 const PORT = 5000;
+
+require("dotenv").config();
+
+const conn = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+conn.connect(err => {
+    if(err) {
+        console.error("Connection error:", err);
+        return;
+    }
+    console.log("Connected to MySQL database!");
+});
+
+/*module.exports = conn;*/
 
 app.use(express.json());
 
