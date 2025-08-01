e4141f1c8127b70c2e805dd486cc39f1308546d6
Omar Bucio <omarbucio2020@gmail.com>
2025-07-31 20:35:26 -0500

    Changed origin to vercel domain for cors

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ console.log(">>> Starting backend app...");
 const app = express();
 
 app.use(cors({
-  origin: true,
+  origin: 'https://hopepaws.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
