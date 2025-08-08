c558c59c7cf47cefec87fbd04f112d8f0b0ad01b
MrPanky <76574822+MrPanky@users.noreply.github.com>
2025-08-08 11:24:18 -0500

    auth fix

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = function (req, res, next) => {
+module.exports = function (req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
   if (!token) return res.status(401).json({ message: 'No token provided' });
